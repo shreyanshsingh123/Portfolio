46b1bc0270ad4c6a7759356b4ab38138f46ba20a
shreyanshsingh123 <91280012+shreyanshsingh123@users.noreply.github.com>
2023-10-15 17:59:20 +0530

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: shreyanshsingh522003@gmail.com</p>
+      <p>Phone: 7007971835</p>
+      <p>LinkedIn:www.linkedin.com/in/shreyansh-singh-79665a224</p>
+    </div>
+  );
+};
+
+export default Contact;
